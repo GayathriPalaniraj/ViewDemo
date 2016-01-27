454edb7b13a877c49802ec77a04179f87098217c
GAYATHRI_P <gayathri1311@gmail.com>
2016-01-27 12:28:42 +0530

    NSObjects

---
+++ Pod/Classes/ViewColor.h
@@ -0,0 +1,15 @@
+//
+//  ViewColor.h
+//  Pods
+//
+//  Created by GAYATHRI_P on 27/01/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ViewColor : NSObject
+
++(void)DemoView:(UIView*)view;
+
+@end
---
+++ Pod/Classes/ViewColor.m
@@ -0,0 +1,19 @@
+//
+//  ViewColor.m
+//  Pods
+//
+//  Created by GAYATHRI_P on 27/01/16.
+//
+//
+
+#import "ViewColor.h"
+#import <UIKit/UIKit.h>
+
+@implementation ViewColor
+
++(void)DemoView:(UIView*)view
+{
+    view.backgroundColor = [UIColor redColor];
+}
+
+@end
